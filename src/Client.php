5598fc48574b33e0bac369732b86c6c45880fcdf
Joel Wurtz <jwurtz@jolicode.com>
2018-01-06 00:08:32 +0100

    Generate 1.33 version

--- src/Client.php
+++ src/Client.php
@@ -34,7 +34,7 @@ class Client extends \Jane\OpenApiRuntime\Client\Psr7HttplugResource
         if (null === $httpClient) {
             $httpClient = \Http\Discovery\HttpClientDiscovery::find();
             $plugins = [];
-            $uri = \Http\Discovery\UriFactoryDiscovery::find()->createUri('v1.32');
+            $uri = \Http\Discovery\UriFactoryDiscovery::find()->createUri('v1.33');
             $plugins[] = new \Http\Client\Common\Plugin\AddPathPlugin($uri);
             $httpClient = new \Http\Client\Common\PluginClient($httpClient, $plugins);
         }
